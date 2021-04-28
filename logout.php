8947245d781c9ad773636795e2b42257048db659
Gaetano Conti <81183559+GaetanoConti@users.noreply.github.com>
2021-04-28 11:17:53 +0200

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+    session_start();
+     if ($_SERVER['REQUEST_METHOD']=="POST") {
+        unset($_SESSION['username']);
+        setcookie("username", "", time() -3600);
+        session_destroy();
+        header("Location: index3.php" );
+    }
+?>
