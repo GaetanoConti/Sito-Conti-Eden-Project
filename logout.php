f26d3a06775744102da5ec67e29692ddc326f000
Gaetano Conti <81183559+GaetanoConti@users.noreply.github.com>
2021-05-16 23:33:08 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-     if ($_SERVER['REQUEST_METHOD']=="POST") {
-        unset($_SESSION['username']);
-        setcookie("username", "", time() -3600);
-        session_destroy();
-        header("Location: index.php" );
-    }
-?>
