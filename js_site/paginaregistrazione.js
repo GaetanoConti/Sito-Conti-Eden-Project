5015e4f486847713667eb2fed3368e4c005dea3c
Gaetano Conti <81183559+GaetanoConti@users.noreply.github.com>
2021-05-28 10:28:52 +0200

    Add files via upload

--- js_site/paginaregistrazione.js
+++ js_site/paginaregistrazione.js
@@ -4,28 +4,12 @@ function onChange() {
                 if (confirm.value === password.value) {
                     confirm.setCustomValidity('');
                 } else {
-                    confirm.setCustomValidity('Passwords do not match');
+                    confirm.setCustomValidity('Le due password non corrispondono');
                 }
                 }
             function validaForm() { 
             if (document.registr.pass.value.length < 8) {
                 alert("Inserire una password di almeno 8 caratteri"); 
-            return false; 
-            }
-            if (document.registr.nome.value=="") { 
-            alert("Inserire nome"); 
-            return false; 
-            }  
-            if (document.registr.cognome.value=="") { 
-            alert("Inserire cognome"); 
-            return false; 
-            }
-            if (document.registr.mail.value=="") { 
-            alert("Inserire email"); 
-            return false; 
-            }
-            if (document.registr.telefono.value=="") { 
-            alert("Inserire numero di telefono"); 
             return false; 
             }
             if (document.registr.telefono.value!="") {
